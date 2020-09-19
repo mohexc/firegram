ab5b1b5906ef19682e877f47b63d592e787cec22
DESKTOP-F6M9PI8\lamun <galunyu@gmail.com>
2020-09-19 16:28:17 +0700

    [Edited] word in TiTleComponent

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Title = () => {
     return (
         <div className="title">
-            <h1>FireGram</h1>
+            <h1>Photo Gallery</h1>
             <h2>Your Pictures</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Create by Nanut.prompiriya</p>
+            <p>Using react-firebase</p>
         </div>
     )
 }
